3ab67c7b0774d8671b62f1feefdb037344d36014
Tessa Harmon <tessa@tessaharmon.com>
2013-08-23 12:53:17 -0400

    Show a progress bar when loading data

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,7 +4,11 @@
 
 angular.module('myApp.controllers', []).
   controller('SongCtrl', ['$scope', '$http', function($scope, $http) {
+    $scope.loading = true;
+
     $http.get('data/songs.json').success(function(data) {
       $scope.songs = data;
+    }).then(function() {
+      $scope.loading = false;
     });
   }]);
