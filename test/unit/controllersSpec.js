bf7d845d9d1f0e788644f4ccc6d13384d9312242
Tessa Harmon <tessa@tessaharmon.com>
2013-08-23 12:42:46 -0400

    Load songs from JSON

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -5,12 +5,24 @@
 describe('controllers', function(){
   beforeEach(module('myApp.controllers'));
 
+  describe('SongCtrl', function() {
+    var scope, ctrl, $httpBackend;
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('data/songs.json').
+        respond([{title: 'The Look of Love'}, {title: 'The Shining'}]);
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
+      scope = $rootScope.$new();
+      ctrl = $controller('SongCtrl', {$scope: scope});
+    }));
+
+    it('should create a "songs" model with 2 songs fetched from xhr', function() {
+      expect(scope.songs).toBeUndefined();
+      $httpBackend.flush();
+
+      expect(scope.songs).toEqual([{title: 'The Look of Love'}, {title: 'The Shining'}]);
+      expect(scope.songs.length).toEqual(2);
+    });
+  });
 });
