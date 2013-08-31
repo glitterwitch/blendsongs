86a189fc440228c24dfac280331852aa306c7e79
Tessa Harmon <tessa@tessaharmon.com>
2013-08-31 00:56:58 -0400

    Enable sorting by column headers.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,7 +8,31 @@ angular.module('myApp.controllers', []).
 
     $http.get('data/songs.json').success(function(data) {
       $scope.songs = data;
+      $scope.orderProp = 'year';
+      $scope.reverse = false;
     }).then(function() {
       $scope.loading = false;
     });
+
+    // Click handler to allow sorting by column. 
+    // Accepts a field name to sort by.
+    $scope.orderSongs = function(field) {
+      if (!$scope.songs || !field) {
+        return;
+      }
+
+      // If we are sorting on the same property again, just do a reverse sort.
+      if ($scope.orderProp === field) {
+        if (!$scope.reverse) {
+          return $scope.reverse = true;
+        }
+
+        return $scope.reverse = false;
+      }
+
+      // Otherwise, set the new order property & sort normally.
+      $scope.orderProp = field;
+      return $scope.reverse = false;
+    }
+
   }]);
