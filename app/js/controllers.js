e4b095fb8daae4df5d51c6889da255fc03d4ef00
Tessa Harmon <tessa@tessaharmon.com>
2013-09-03 22:08:25 -0400

    Break the firebase DB reference into a service

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,66 +3,64 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('SongCtrl', ['$scope', 'angularFire', 'fireBaseURL', '$location', function($scope, $songRetriever, $fireBaseURL, $location) {
-    $scope.loading = true;
-    $scope.orderProp = 'dateCreated';
-    $scope.reverse = true;
+  controller('SongCtrl', ['$scope', 'BlendSongsDB', 'angularFire', 
+    function($scope, $db, angularFire) {
+      $scope.loading = true;
+      $scope.orderProp = 'dateCreated';
+      $scope.reverse = true;
 
-    var firebase = new Firebase($fireBaseURL);
+      angularFire($db, $scope, 'songs').then(function() {
+        $scope.loading = false;
+      });
 
-    $songRetriever(firebase, $scope, 'songs').then(function() {
-      $scope.loading = false;
-    });
-
-    // Click handler to allow sorting by column. 
-    // Accepts a field name to sort by.
-    $scope.orderSongs = function(field) {
-      if (!$scope.songs || !field) {
-        return;
-      }
-
-      // If we are sorting on the same property again, just do a reverse sort.
-      if ($scope.orderProp === field) {
-        if (!$scope.reverse) {
-          return $scope.reverse = true;
+      // Click handler to allow sorting by column. 
+      // Accepts a field name to sort by.
+      $scope.orderSongs = function(field) {
+        if (!$scope.songs || !field) {
+          return;
         }
 
+        // If we are sorting on the same property again, just do a reverse sort.
+        if ($scope.orderProp === field) {
+          if (!$scope.reverse) {
+            return $scope.reverse = true;
+          }
+
+          return $scope.reverse = false;
+        }
+
+        // Otherwise, set the new order property & sort normally.
+        $scope.orderProp = field;
         return $scope.reverse = false;
       }
 
-      // Otherwise, set the new order property & sort normally.
-      $scope.orderProp = field;
-      return $scope.reverse = false;
-    }
-
-    $scope.routeToSong = function(index) {
-      $location.path("/songs/" + index);
-    }
-
-  }]).
-  controller('SongAddCtrl', ['$scope', 'angularFire', 'fireBaseURL', function($scope, $songRetriever, $fireBaseURL) {
-    var firebase = new Firebase($fireBaseURL);
-    $scope.songs = $songRetriever(firebase, $scope, 'songs');
-
-    $scope.formChange = function() {
-      if ($scope.success) {
-        $scope.success = false;
+      $scope.routeToSong = function(index) {
+        $location.path("/songs/" + index);
       }
-    };
+    }]).
+  controller('SongAddCtrl', ['$scope', 'BlendSongsDB', 'angularFire', 
+    function($scope, $db, angularFire) {
+      $scope.songs = angularFire($db, $scope, 'songs');
 
-    $scope.addSong = function(data) {
-      data.year = parseInt(data.year, 10); // Convert year to int so sorting will work properly.
-      data.dateCreated = new Date().getTime(); // Add a timestamp
-      $scope.songs.push(data);
+      $scope.formChange = function() {
+        if ($scope.success) {
+          $scope.success = false;
+        }
+      };
 
-      // Reset the form
-      $scope.song = angular.copy({
-        title: null,
-        artist: null,
-        year: null,
-        theme: null
-      });
+      $scope.addSong = function(data) {
+        data.year = parseInt(data.year, 10); // Convert year to int so sorting will work properly.
+        data.dateCreated = new Date().getTime(); // Add a timestamp so newly-added songs can appear at the top of the list.
+        $scope.songs.push(data);
 
-      $scope.success = true;
-    };
-  }]);
+        // Reset the form
+        $scope.song = angular.copy({
+          title: null,
+          artist: null,
+          year: null,
+          theme: null
+        });
+
+        $scope.success = true;
+      };
+    }]);
--- app/js/services.js
+++ app/js/services.js
@@ -2,4 +2,7 @@
 
 /* Services */
 angular.module('myApp.services', []).
-  value('fireBaseURL', 'https://blendsongs.firebaseio.com/');
+  value('fireBaseURL', 'https://blendsongs.firebaseio.com/').
+  factory('BlendSongsDB', ['angularFire', 'fireBaseURL', function(angularFire, fireBaseURL) {
+    return new Firebase(fireBaseURL);
+  }]);
