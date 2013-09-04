ee05fbfc58f8708d653d6c66081822f9bce4aa9b
Tessa Harmon <tessa@tessaharmon.com>
2013-09-03 22:45:03 -0400

    Formatting

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,8 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.directives', 'myApp.services', 'firebase', 'SongCtrl', 'SongAddCtrl']).
+angular.module('myApp', 
+  ['myApp.filters', 'myApp.directives', 'myApp.services', 'firebase', 'SongCtrl', 'SongAddCtrl']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/songs', {templateUrl: 'partials/songs.html', controller: 'SongCtrl'});
     $routeProvider.when('/songs/add', {templateUrl: 'partials/song-add.html', controller: 'SongAddCtrl'});
